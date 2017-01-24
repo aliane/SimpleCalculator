aff3587768776ace2fa60d673600ddcbc4e9734a
warihanagumah <warihana.gumah@ashesi.edu.gh>
2017-01-24 12:07:16 +0000

    The division

--- SimpleCalculator.java
+++ SimpleCalculator.java
@@ -1,45 +1,46 @@
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class SimpleCalculator {
+  
+  public static void main(String[] args) {
+    String operation = "";
+    double n1 = 0;
+    double n2 = 0;
     
-    public static void main(String[] args) {
-        double n1, n2;
-        String operation;
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("Enter first number");
-        n1 = input. nextDouble();
-
-        System.out.println("Enter second number");
-        n2 = input. nextDouble();
-
-        Scanner op = new Scanner(System.in);
-        System.out.println("Enter your operation");
-        operation = op.next();
-
-        switch (operation)  {
-        case "+":
-            System.out.println("Your answer is " + (n1 + n2));
-            break;
-
-        case "-":
-            System.out.println("Your answer is " + (n1 - n2));
-            break;
-
-        case "/":
-          if(n2!=0)
-            System.out.println("Your answer is " + (n1 / n2));
-          else
-            System.out.println(" you can't devide a number by zero");
-            break;
-
-        case "*":
-            System.out.println("Your asnwer is " + (n1 * n2));
-            break;
-
-        default:
-            System.out.println("Use a correct operator");
-
-        }
+    for (int i=0; i<args.length; i++){
+      n1 = Double.parseDouble(args[0]);
+      n2 = Double.parseDouble(args[2]);
+      operation = args[1];
+    } 
+    
+    switch (operation){
+      //add two numbers
+      case "+":
+        System.out.println("Your answer is " + (n1 + n2));
+        break;
+      
+        //subtract two numbers
+      case "-":
+        System.out.println("Your answer is " + (n1 - n2));
+        break;
+       
+        //divide two numbers
+      case "/":
+        if(n2!=0)
+        System.out.println("Your answer is " + (n1 / n2));
+        else
+          System.out.println(" you can't devide a number by zero");
+        break;
+        
+      //multiply two numbers
+      case "*":
+        System.out.println("Your asnwer is " + (n1 * n2));
+        break;
+        
+       //print this when all cases does not apply 
+      default:
+        System.out.println("Use a correct operator");
+        break;
     }
+  }
 }
